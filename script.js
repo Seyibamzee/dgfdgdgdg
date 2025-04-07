f9a7cc5a63cf75e2e5b8d27acb41567c0de0758f
Seyibamzee <okalabi@cps.edu>
2025-04-07 14:30:35 +0000

    vjkvjvjbfbg

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let SorS = document.querySelector(".SorS");
-console.log(SorS);
+
 let RorS = document.querySelector(".RorS");
-console.log(RorS);
+
 let check = document.querySelector(".check");
-console.log(check);
+
 let result= document.querySelector(".result");
+let answer= document.querySelector(".answer");
+
+
+
+ let MoonDongeun = document.querySelector(".MoonDongeun")
+ let JeonJaejoon = document.querySelector(".JeonJaejoon")
+ let HaDoyeong = document.querySelector(".HaDoyeong")
+ let ParkYeonjin = document.querySelector(".ParkYeonjin")
 
 
 
@@ -19,20 +27,28 @@ console.log(SorSA);
 
     if (SorSA === "Sweet" && RorSA === "Rainy"){
         console.log ("Moon Dong-eun")
-    result.innerHTML= "You are Moon Dong-eun";
+        result.innerHTML="You are Moon Dong-eun"
+    answer.src="moon.jpg"
+      
 
 }else if (SorSA === "Sour" && RorSA === "Rainy"){
     console.log ("Jeon Jae-joon")
-result.innerHTML= "You are Jeon Jae-joon";
+result.innerHTML= "You are Jeon Jae-joon"
+answer.src="jeon.jpg"
+
+
 
 }else if (SorSA === "Sweet" && RorSA === "Sunny"){
     console.log ("Ha Do-yeong")
-result.innerHTML= "You are Ha Do-yeong";
+result.innerHTML= "You are Ha Do-yeong"
+answer.src="hado.jpg"
 
 }else if (SorSA === "Sour" && RorSA === "Sunny"){
     console.log ("Park Yeon-jin")
 result.innerHTML= "You are Park Yeon-jin"
-};
+answer.src="park.webp"
 }
+}
+
 
 
